8b120ff6baa7ee38a3de353d93c72bee22a71b41
Paul Hummer <paul@eventuallyanyway.com>
2012-12-13 13:36:45 -0700

    I hate you git.

--- lastfmapi.py
+++ lastfmapi.py
@@ -42,7 +42,6 @@ class LastFmApi(object):
 
         params = urllib.urlencode(params)
         request = urllib2.Request(LASTFM_API_ENDPOINT, params, headers)
-        import pdb; pdb.set_trace()
         response = urllib2.urlopen(request).read()
 
         s = json.loads(response)
