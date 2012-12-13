cd95ad2075ca1533f8d1eed6a4d656e12e2b24e2
Paul Hummer <paul@eventuallyanyway.com>
2012-12-13 13:10:35 -0700

    Facepalm. Be careful when naming variables

--- lastfmapi.py
+++ lastfmapi.py
@@ -33,7 +33,7 @@ class LastFmApi(object):
         return generic_request
 
     def __send(self, params):
-        params['api_key'] = self.__key
+        params['api_key'] = self.__api_key
         params['format'] = 'json'
 
         headers = {
@@ -42,6 +42,7 @@ class LastFmApi(object):
 
         params = urllib.urlencode(params)
         request = urllib2.Request(LASTFM_API_ENDPOINT, params, headers)
+        import pdb; pdb.set_trace()
         response = urllib2.urlopen(request).read()
 
         s = json.loads(response)
