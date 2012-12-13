1d892af434a00161ce342955d07fffebe4fef797
Paul Hummer <paul@eventuallyanyway.com>
2012-12-13 13:46:04 -0700

    Add setup.py and ignore junk

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    author='Paul Hummer',
+    author_email='paul@eventuallyanyway.com',
+    description='A thin wrapper for accessing the Last.fm API',
+    keywords='last.fm api',
+    license='http://www.opensource.org/licenses/mit-license.php',
+    name='lastfmapi',
+    py_modules=['lastfmapi'],
+    url='https://github.com/rockstar/lastfmapi',
+    version='0.1',
+)
